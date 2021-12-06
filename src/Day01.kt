7ddf435e2f48db3dfb01fc183d3f18a6b3d58186
tgortworst <t.gortworst@tm-pro.eu>
2021-12-06 21:08:00 +0100

    day 01 part 2

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,23 +1,24 @@
 fun main() {
-    fun part1(input: List<String>): Int {
-        val numberInput = input.map { it.toInt() }
-        val iterator = numberInput.listIterator(1) // skip first index since it has no previous
+    fun part1(input: List<Int>): Int {
+        val iterator = input.listIterator(1) // skip first index since it has no previous
         var counter = 0
 
-        while (iterator.hasNext()) if (numberInput[iterator.previousIndex()] < iterator.next()) counter++
+        while (iterator.hasNext()) if (input[iterator.previousIndex()] < iterator.next()) counter++
 
         return counter
     }
 
-    fun part2(input: List<String>): Int {
-        return input.size
-    }
+    fun part2(input: List<Int>) = input
+        .windowed(3)
+        .map { it.sum() }
+        .windowed(2)
+        .count { it.last() > it.first() }
 
     // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
+    val testInput = readIntInput("Day01_test")
     check(part1(testInput) == 1)
 
-    val input = readInput("Day01")
+    val input = readIntInput("Day01")
     println(part1(input))
     println(part2(input))
 }
