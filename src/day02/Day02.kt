d9cd8340b8a8b602a2c3a2c8f53481527a2c1d88
tgortworst <t.gortworst@tm-pro.eu>
2021-12-07 14:33:21 +0100

    day 2 part 2

--- src/day02/Day02.kt
+++ src/day02/Day02.kt
@@ -3,13 +3,13 @@ package day02
 import readInput
 
 fun main() {
+    fun splitInput(input: List<String>) = input.map { it.split(" ") }
+
     fun part1(input: List<String>): Int {
         var y = 0 // depth
         var x = 0 // horizontal position
 
-        input.map {
-            it.split(" ")
-        }.forEach {
+        splitInput(input).forEach {
             val speed = it.last().toInt()
 
             when (it.first()) {
@@ -22,6 +22,28 @@ fun main() {
         return x * y
     }
 
+    fun part2(input: List<String>): Int {
+        var y = 0 // depth
+        var x = 0 // horizontal position
+        var aim = 0
+
+        splitInput(input).forEach {
+            val speed = it.last().toInt()
+
+            when (it.first()) {
+                "forward" -> {
+                    x += speed
+                    y += (aim * speed)
+                }
+                "up" -> aim -= speed
+                "down" -> aim += speed
+            }
+        }
+
+        return x * y
+    }
+
     val input = readInput("Day02")
     println(part1(input))
+    println(part2(input))
 }
